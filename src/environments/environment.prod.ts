130a9676a4615e5a277ccb42d895842097e73c2e
Tirasz <zacsi143@gmail.com>
2022-05-02 15:23:28 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: '*************************',
-    appId: '************************',
-    storageBucket: '***********************',
-    apiKey: '**************************',
-    authDomain: '***********************',
-    messagingSenderId: '***************',
+    projectId: 'torrent-garfield',
+    appId: '1:175622383732:web:a56dabe2324f5f8e078921',
+    storageBucket: 'torrent-garfield.appspot.com',
+    apiKey: 'AIzaSyA6nTDd5p_ixg4qSX2yLXxDwfHb3a9lI0c',
+    authDomain: 'torrent-garfield.firebaseapp.com',
+    messagingSenderId: '175622383732',
   },
   production: true
 };
